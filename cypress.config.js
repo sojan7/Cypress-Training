5c19987a33f3787caa4ae28750b9c1e836d5923e
Sojan Somarajan <sojan_somarajan@epam.com>
2024-08-13 19:39:36 +0530

    Cypress_Initial_push

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  downloadsFolder: "cypress/downloads",
+  fixturesFolder: "cypress/fixtures",
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
+
+  defaultCommandTimeout: 4000,
+  pageLoadTimeout: 60000,
+  requestTimeout: 5000,
+  responseTimeout: 30000,
+
+  e2e: {
+    baseUrl: "https://www.saucedemo.com/",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,21 @@
+describe("template spec", () => {
+  before(function () {
+    //
+  });
+
+  after(function () {
+    //
+  });
+
+  afterEach(function () {
+    //
+  });
+
+  beforeEach(function () {
+    //
+  });
+
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
+  });
+});
