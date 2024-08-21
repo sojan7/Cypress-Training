a8b437a754304d86a5967c12f426680f5c3aacdf
Sojan Somarajan <sojan_somarajan@epam.com>
2024-08-21 19:21:50 +0530

    Base URL Changed and Cypress Updated to the latest version

--- cypress.config.js
+++ cypress.config.js
@@ -13,8 +13,6 @@ module.exports = defineConfig({
 
   e2e: {
     baseUrl: "https://demoqa.com/",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,5 @@
 describe("template spec", () => {
   it("passes", () => {
-    const baseUrl = Cypress.config("baseUrl");
-    cy.visit(baseUrl);
-
-    // Alternatively, you can also directly visit using the baseUrl without explicitly retrieving it
-    // cy.visit('/');
+    cy.visit(Cypress.config("baseUrl"));
   });
 });
